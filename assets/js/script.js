486715b04d24d57c114076e990a41f62867f26d5
Vasil Kotsev <vasil.kotsev@gmail.com>
2018-09-27 19:57:36 +0300

    add scroll event functionality to header
    add scroll event JQ/JS functionality to header to stick it on the widnow top

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
 
     /* Hamburger menu icon */
 
-    var menu = $('.menu');
-    var header = $('#header');
-    var hamburger = header.find('.hamburger');
+    let menu = $('.menu');
+    let header = $('#header');
+    let hamburger = header.find('.hamburger');
 
 
     hamburger.on('click', function () {
@@ -17,6 +17,33 @@ $(document).ready(function () {
         e.preventDefault();
         menu.removeClass('active');
         hamburger.removeClass('hamburger--stand is-active');
+    });
+
+
+    /*Scroll event functionallity for fixed header */
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 0) {
+            $('header').addClass('header-fixed');
+            $('header').removeClass('header-absolute');
+            $('path#Fill-1').addClass('pink-fill-svg');
+            $('path#Fill-1').removeClass('st0');
+            $('#logo span, .menu a').addClass('pink-font-color');
+            $('#logo span, .menu a').removeClass('white-font-color');
+            $('.menu__button').addClass('pink-background-color');
+
+        }
+
+        else {
+            $('header').removeClass('header-fixed');
+            $('header').addClass('header-absolute');
+            $('path#Fill-1').addClass('st0');
+            $('path#Fill-1').removeClass('pink-fill-svg');
+            $('#logo span, .menu a').addClass('white-font-color');
+            $('#logo span, .menu a').removeClass('pink-font-color');
+            $('.menu__button').removeClass('pink-background-color');
+           
+        }
     });
 
 });
